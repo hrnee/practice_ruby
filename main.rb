13cd10b7a24a093fefa73b3802d7b42f0aa26ef8
hrnee <blue-yg.spsou@gmail.com>
2015-11-10 16:15:29 +0900

    add color cyan

--- main.rb
+++ main.rb
@@ -1,9 +1,9 @@
 
 require "./lib/actor/human"
 #require "./lib/decorator/character"
-require "./lib/decorator/color_red"
+require "./lib/decorator/color_cyan"
 
 actor = Actor::Human.new 'My name is Hoge Hoge'
 #decorator = Decorator::Character.new actor
-decorator = Decorator::ColorRed.new actor
+decorator = Decorator::ColorCyan.new actor
 puts decorator.show_message
